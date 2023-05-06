09652c3431e331be7bc834a56f22819267cd0d95
mehmettokgoz <mehmet.tokgoz@hazelcast.com>
2023-05-06 22:41:00 +0300

    Refactor GUI application.

--- sdk/client_gui.py
+++ sdk/client_gui.py
@@ -7,58 +7,83 @@ from tkinter import *
 
 from tcp.monopoly_client import MonopolyClient
 
-port = int(sys.argv[1])
-# TODO: This variable should be thread-safe
-client = MonopolyClient(port)
+client: MonopolyClient = None
 
 
-class Window:
-
-    def show_entry_fields(self):
-        print(("input %s" % (self.e1.get())))
-        client.send_command(self.e1.get())
-
-    def update_window(self):
-        self.subframe = Frame(self.master, background="blue")
-        self.subject = Label(self.subframe, text="Subject")
-        self.subject.place(relx=0.5, rely=0.5, anchor=CENTER)
-        self.subframe.pack(expand=True, fill=BOTH)
-
-        self.scrollbar = tk.Scrollbar(self.subframe, orient="vertical")
-        self.lb = tk.Listbox(self.subframe, width=50, height=10, yscrollcommand=self.scrollbar.set)
-        self.scrollbar.config(command=self.lb.yview)
-
-        self.scrollbar.pack(side="right", fill="y")
-        self.lb.pack(side="left", fill="both", expand=True)
-
-        self.subframe2 = Frame(self.master, background="gray")
-
-        tk.Label(self.subframe2, text="command: ").grid(row=0)
-        self.e1 = tk.Entry(self.subframe2)
-        self.e1.grid(row=0, column=1)
-        tk.Button(self.subframe2, text='send', command=self.show_entry_fields).grid(row=0, column=2, sticky=tk.W, pady=4)
-        self.subframe2.pack(expand=True, fill=BOTH)
+class GUI:
+    master: Tk
+    up_frame: Frame
+    below_frame: Frame
+    scroll_bar: Scrollbar
+    list_box: Listbox
+    entry: Entry
+    client_connected = False
+    port = 0
 
     def __init__(self, master):
         self.master = master
         self.update_window()
+
+    def handle_start(self, port):
+        self.port = port
+        # TODO: This variable should be thread safe
+        global client
+        client = MonopolyClient(port)
+        self.list_box.insert("end", "Client is connected to server.")
+        self.client_connected = True
         t = Thread(target=self.add_new_log)
         t.start()
+
+    def show_entry_fields(self):
+        i = self.entry.get()
+        if not self.client_connected:
+            args = i.split(",")
+            if args[0] == "connect":
+                self.handle_start(int(args[1]))
+                self.entry.delete(0, END)
+        else:
+            self.entry.delete(0, END)
+            client.send_command(i)
+
+    def handle_enter(self, event):
+        self.show_entry_fields()
+
+    def update_window(self):
+        self.up_frame = Frame(self.master, background="red")
+
+        self.up_frame.pack(expand=True, fill=BOTH)
+
+        self.scroll_bar = tk.Scrollbar(self.up_frame, orient="vertical")
+        self.list_box = tk.Listbox(self.up_frame, width=50, height=20, background="gray31",
+                                   yscrollcommand=self.scroll_bar.set)
+        self.scroll_bar.config(command=self.list_box.yview)
+        self.list_box.insert("end", "Welcome to Monopoly Client GUI.")
+        self.scroll_bar.pack(side="right", fill="y")
+        self.list_box.pack(side="left", fill="both", expand=True)
+
+        self.below_frame = Frame(self.master, background="black")
+        tk.Label(self.below_frame, text="command: ", background="black").grid(row=0, column=0)
+        self.entry = tk.Entry(self.below_frame, background="black")
+        self.entry.grid(row=0, column=1)
+        self.entry.bind('<Return>', self.handle_enter)
+        tk.Button(self.below_frame, text='send', background="black", command=self.show_entry_fields).grid(row=0,
+                                                                                                          column=2,
+                                                                                                          stick=tk.W,
+                                                                                                          pady=3)
+        self.below_frame.pack(expand=False, fill=X)
 
     def add_new_log(self):
         i = 0
         client.c.acquire()
-        self.lb.insert("end", "starting to wait for client c")
+        self.list_box.insert("end", f"Client is listening localhost:{self.port}")
         while True:
-            # time.sleep(1)
-            # Wait for new log to arrive - client.c is notified inside the MonopolyClient
             client.c.wait()
-            print("this thread waked up!")
-            self.lb.insert("end", client.logs.pop())
+            print("Condition variable is notified! New log is arrived from server.")
+            self.list_box.insert("end", client.logs.pop())
             i += 1
 
 
 if __name__ == "__main__":
     root = Tk()
-    window = Window(root)
+    window = GUI(root)
     root.mainloop()
