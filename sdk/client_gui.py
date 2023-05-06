be2e9f2b14bb37f5cc7519502d6085d62a948099
fazlibalkan <fazlibalkan@outlook.com>
2023-05-06 23:41:46 +0300

    gui style change

--- sdk/client_gui.py
+++ sdk/client_gui.py
@@ -55,18 +55,18 @@ class GUI:
 
         self.scroll_bar = tk.Scrollbar(self.up_frame, orient="vertical")
         self.list_box = tk.Listbox(self.up_frame, width=50, height=20, background="gray31",
-                                   yscrollcommand=self.scroll_bar.set)
+                                   yscrollcommand=self.scroll_bar.set, fg='#fff')
         self.scroll_bar.config(command=self.list_box.yview)
         self.list_box.insert("end", "Welcome to Monopoly Client GUI.")
         self.scroll_bar.pack(side="right", fill="y")
         self.list_box.pack(side="left", fill="both", expand=True)
 
         self.below_frame = Frame(self.master, background="black")
-        tk.Label(self.below_frame, text="command: ", background="black").grid(row=0, column=0)
-        self.entry = tk.Entry(self.below_frame, background="black")
+        tk.Label(self.below_frame, text="command: ", background="black", fg='#fff').grid(row=0, column=0)
+        self.entry = tk.Entry(self.below_frame, background="black", fg='#fff')
         self.entry.grid(row=0, column=1)
         self.entry.bind('<Return>', self.handle_enter)
-        tk.Button(self.below_frame, text='send', background="black", command=self.show_entry_fields).grid(row=0,
+        tk.Button(self.below_frame, text='send', background="black", command=self.show_entry_fields, fg='#fff').grid(row=0,
                                                                                                           column=2,
                                                                                                           stick=tk.W,
                                                                                                           pady=3)
